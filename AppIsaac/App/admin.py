fd76434fff75e8259be2b78f483cf65787dc0c08
Gon <gonzalo8457@gmail.com>
2023-10-22 18:37:16 -0300

    Clases en views

--- AppIsaac/App/admin.py
+++ AppIsaac/App/admin.py
@@ -4,3 +4,6 @@ from .models import Item, Objeto, Usuario
 admin.site.register(Item)
 admin.site.register(Objeto)
 admin.site.register(Usuario)
+
+def __str__(self):
+    return f"Nombre: {self.nombre} - Personaje: {self.personaje} - Mail: {self.email}"
--- AppIsaac/App/models.py
+++ AppIsaac/App/models.py
@@ -11,8 +11,14 @@ class Item(models.Model):
     nombre = models.CharField(max_length=40)
     precio = models.IntegerField()
     
+    def __str__(self) -> str:
+        return f"Nombre: {self.nombre} - Precio: {self.precio}"
+    
 class Usuario(models.Model):
     nombre = models.CharField(max_length=40)
     personaje = models.CharField(max_length=20)
     email = models.EmailField(max_length=40)
+    
+    def __str__(self) -> str:
+        return f"Nombre: {self.nombre} - Personaje: {self.personaje} - Email: {self.email}"
 
